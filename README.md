a8d56fe55a11d44c31933f698c80a72781ff0b31
Ravi Patel <rp70960@gmail.com>
2021-04-09 16:34:49 -0400

    The SqlAlchemy is donw with the bonus and comments

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # SQLAlchemy_Challenge
+
+The SQL Alchemy Challenge is completed in just the way asked to do.
+The folder contain the jupyter notebook for the climate starter and the bonus part I and part II.
+
+The app.py file contain the flask method and all the routes required for.
+
+In the app.py file when runned the last route which does the same thing as the start_date route does, but this takes an user input for the process you run.
+
+While running the last routes pleas keep and eye on the terminal where it will ask to enter the start date.
+
+P.S: Running only that part of the code may required to re-run the code again. Other part of the code will run smooth in the single try.
--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from flask import Flask, jsonify
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite?check_same_thread=False")
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -33,17 +33,23 @@ app = Flask(__name__)
 # Flask Routes
 #################################################
 
+
+# First main page with all routes..
 @app.route("/")
 def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/start<br/>"
-        f"/api/v1.0/start_end"
+        f"For all Precipitation: /api/v1.0/precipitation <br/>"
+        f"For all the Station: /api/v1.0/stations <br/>"
+        f"For the temperature of the most active station: /api/v1.0/tobs <br/>"
+        f"For the temperature from the start date and so on: /api/v1.0/<start_date> <br/>"
+        f"For the temperature between the start date and end date: /api/v1.0/<start_date>/<end_date> <br/>"
+        f"For the temperature from the start date by givining input into the terminal(another method for getting temperature): /api/v1.0/start"
+        
     )
+
+# Precipitation page.
 
 @app.route("/api/v1.0/precipitation")
 def precip():
@@ -65,6 +71,7 @@ def precip():
         all_precipitation.append(precip_df)
     return jsonify(all_precipitation)
 
+# Display all the stations. 
 
 @app.route("/api/v1.0/stations")
 def station():
@@ -73,6 +80,8 @@ def station():
     session.close()
     all_station = list(np.ravel(stat))
     return jsonify(all_station)
+
+# Dispaly the temperatur of the most active station.
 
 @app.route("/api/v1.0/tobs")
 def temperature():
@@ -88,28 +97,56 @@ def temperature():
     all_station_temp = list(np.ravel(station_temp))
     return jsonify(all_station_temp)
 
-@app.route("/api/v1.0/start")
-def start():
+
+# Dispaly the temperature from the start date.
+
+@app.route("/api/v1.0/<date_start>")
+def start(date_start):
     session = Session(engine)
-    date = session.query(Measure.date).order_by(asc(Measure.date)).first()
-    start_date = dt.date(2010,1,1)
+    #date = session.query(Measure.date).order_by(asc(Measure.date)).first()
+    #start_date = dt.date(2010,1,1)
     temp_data = session.query(func.min(Measure.tobs), func.max(Measure.tobs), func.avg(Measure.tobs)).\
-    filter(Measure.date >=start_date).all()
+    filter(Measure.date >=date_start).all()
     session.close()
     all_temp_data = list(np.ravel(temp_data))
-    return f"The Lowest temperature Since {start_date} is {all_temp_data[0]}.<br/> The Highest temperature since {start_date} is {all_temp_data[1]}.<br/> The Average Temperature since {start_date} is {all_temp_data[2]}."
+    return f"The Lowest temperature Since {date_start} is {all_temp_data[0]}.<br/> The Highest temperature since {date_start} is {all_temp_data[1]}.<br/> The Average Temperature since {date_start} is {all_temp_data[2]}."
 
+# Display the temperature between two dates.
 
-@app.route("/api/v1.0/start_end")
-def start_end():
+@app.route("/api/v1.0/<start_date>/<end_date>")
+def start_end(start_date,end_date):
     session = Session(engine)
-    start_date = dt.date(2010,1,1)
-    end_date = dt.date(2010,12,31)
+    #start_date = dt.date(2010,1,1)
+    #end_date = dt.date(2010,12,31)
     one_year_temp_data = session.query(func.min(Measure.tobs), func.max(Measure.tobs), func.avg(Measure.tobs)).\
     filter(Measure.date >= start_date).filter(Measure.date <= end_date).all()
     session.close()
     one_year_data = list(np.ravel(one_year_temp_data))
     return f"The Lowest temperature between {start_date} and {end_date} was {one_year_data[0]}.<br/> The Highest temperature between {start_date} and {end_date} was {one_year_data[1]}.<br/> The Average Temperature between {start_date} and {end_date} was {one_year_data[2]}."
+
+# Display the temperature from the start date.
+
+# It's an another method where user input is considered into the terminal. 
+
+# When you do this part keep a watch in the terminal where it will ask for the date input. It might take 2-3 tries to run but it works perfect.
+
+# Once you will enter the route into the search bar come back to the terminal down below it will ask you to enter the date.
+
+# enter the date in format 2010-01-01 year-month-date without any "".
+
+# Than go back to the localhost link it will show the result. If not try to rerun the code. It will show for sure.
+
+@app.route("/api/v1.0/start")
+def start_try():
+    session = Session(engine)
+    def temp_start(start_date_try):
+
+        return session.query(func.min(Measure.tobs), func.max(Measure.tobs), func.avg(Measure.tobs)).filter(Measure.date >=start_date_try).all()
+    date_input = input("Enter the start date: ")
+    all_temp_data = temp_start(date_input)
+    all_t = list(np.ravel(all_temp_data))
+    session.close()
+    return f"The Lowest temperature Since {date_input} is {all_t[0]}.<br/> The Highest temperature since {date_input} is {all_t[1]}.<br/> The Average Temperature since {date_input} is {all_t[2]}."
 
 
 if __name__ == '__main__':
